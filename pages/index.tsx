a6c6b77aaf777ff41df8849bea94bca12c1c8973
Phil Smithies <philsmithies@gmail.com>
2022-02-18 08:29:10 +0100

    db seeded and tailwind for prototyping front end

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,10 @@ const Home: NextPage = () => {
   return (
     <Container>
       <Box paddingY={5}>
-        <Typography variant="h1" gutterBottom>
-          Servus World!
-        </Typography>
-        <Typography variant="h2">
+        <h1 className="font-4xl text-white">Servus World!</h1>
+        <h2 className="font-3xl text-white">
           {`Now it's your turn. If you see this, everything is setup.`}
-        </Typography>
+        </h2>
       </Box>
     </Container>
   );
