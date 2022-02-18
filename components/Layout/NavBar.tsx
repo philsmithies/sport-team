78d0a35f985ac7de4929acf1cfe9e16e77a52e7a
Phil Smithies <philsmithies@gmail.com>
2022-02-18 09:00:18 +0100

    layout with rough navbar and minimal styling

---
+++ components/Layout/NavBar.tsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <div className="flex h-14 w-full items-center justify-center bg-yellow-400 text-3xl">
+      Sport Thieme Coaches
+    </div>
+  );
+};
+
+export default NavBar;
