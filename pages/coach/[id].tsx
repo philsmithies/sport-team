70f936214e5ee56c4f83f6a57f16672625eac6e4
Phil Smithies <philsmithies@gmail.com>
2022-02-18 10:29:32 +0100

    coach profile page

---
+++ pages/coach/[id].tsx
@@ -0,0 +1,47 @@
+import prisma from "../../lib/prisma";
+
+const Coach = ({ coach }) => {
+  return (
+    <div className="container mx-auto">
+      <div className="mt-20 flex h-72 flex-col items-center justify-center bg-red-400">
+        <h1 className="text-4xl">The Profile Page of: {coach.name}</h1>
+        <p className="mt-2">Phone:{coach.phone}</p>
+        <p>Website:{coach.website}</p>
+        <p>Specialties:</p>
+        {coach.specialties.map((skill) => {
+          return (
+            <p
+              key={skill.id}
+              className="mt-2 rounded border-2 bg-white py-1 px-2"
+            >
+              {skill.name}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Coach;
+
+export const getServerSideProps = async ({ params }) => {
+  const id = parseInt(params.id);
+  console.log(id);
+  const coach = await prisma.coach.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      email: true,
+      name: true,
+      phone: true,
+      website: true,
+      specialties: true,
+    },
+  });
+  return {
+    props: {
+      coach,
+    },
+  };
+};
