f1cfa6931cda506f9596cb4a79c9ce8951122cd9
Phil Smithies <philsmithies@gmail.com>
2022-02-21 07:17:47 +0100

    profile details UI

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Sport-Thieme displays a dynamic list of sports coaches on the website. In order 
 The first 5 todos are required, all the others are bonus tasks. Please don't change anything backend related and use the graph as shipped in /pages/api.
 
 - [x] Write graphql queries against https://localhost:3000/api<br>Use your queries manually or with the already installed codegen `yarn generate:types`.<br>You got one query in the graphql folder for free.
-- [ ] Create a UI to list all our coaches at Sport-Thieme with the Material-UI components (already installed)
+- [x] Create a UI to list all our coaches at Sport-Thieme with the Material-UI components (already installed)
 - [ ] Create a UI for a single coach with the Material-UI components (already installed)
 - [x] Implement at least one mutation updating the coaches
 - [x] Implement a mutation for adding specialties to the coach
--- components/ProfilePage/ProfileDetails.tsx
+++ components/ProfilePage/ProfileDetails.tsx
@@ -6,13 +6,12 @@ import { SINGLE_COACH } from "../../graphql/singleCoach";
 import Router from "next/router";
 import { Grid, Typography, Container, Button, Box, Chip } from "@mui/material";
 import SportsFootballIcon from "@mui/icons-material/SportsFootball";
+import Image from "next/image";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import UpdateIcon from "@mui/icons-material/Update";
 
 const ProfileDetails = ({ coach }) => {
-  const { error, data } = useQuery(SINGLE_COACH, {
-    variables: { where: { id: coach.id } },
-  });
   const [removeSpecialty] = useMutation(REMOVE_SPECIALTY);
-
   const handleRemove = (specialtyId) => {
     try {
       removeSpecialty({
@@ -29,23 +28,6 @@ const ProfileDetails = ({ coach }) => {
     Router.reload();
   };
 
-  if (error)
-    return (
-      <Grid
-        container
-        sx={{
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          minHeight: "60vh",
-        }}
-      >
-        <Typography variant="h1">
-          Looks like there was a problem: {error.message}
-        </Typography>
-      </Grid>
-    );
-
   return (
     <Container
       maxWidth="md"
@@ -53,37 +35,87 @@ const ProfileDetails = ({ coach }) => {
         marginTop: 5,
         marginBottom: 20,
         border: 2,
-        display: "flex",
-        flexDirection: "column",
       }}
     >
-      <Box>
-        <Link href="/" passHref>
-          <Button variant="contained" size="large" sx={{ maxWidth: 300 }}>
-            Back to all Coaches
-          </Button>
-        </Link>
-      </Box>
-      <h1 className="text-4xl">Coach: {coach.name}</h1>
-      <p className="mt-2">Phone:{coach.phone}</p>
-      <p>Website:{coach.website}</p>
-      <p>Email:{coach.email}</p>
-      <p>Specialties:</p>
-      <div className="flex w-4/12 justify-between">
-        {data?.coach.specialties.map((specialty) => {
-          return (
-            <Chip
-              icon={<SportsFootballIcon />}
-              key={specialty.id}
-              label={specialty.name}
-              component="a"
-              variant="outlined"
-              sx={{ height: 32 }}
-              onDelete={(e) => handleRemove(specialty.id)}
-            />
-          );
-        })}
-      </div>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          maxWidth: 640,
+        }}
+      >
+        <Button
+          variant="contained"
+          size="large"
+          sx={{
+            marginTop: 4,
+            marginBottom: 4,
+            alignSelf: "flex-start",
+          }}
+        >
+          <ArrowBackIcon sx={{ marginRight: 1 }} />
+          Back
+        </Button>
+        <Image
+          src="/images/profile-default.png"
+          alt="me"
+          layout="fixed"
+          width="150"
+          height="150"
+          className="profile-image"
+        />
+        <Typography variant="h5" sx={{ marginTop: 3, marginBottom: 2 }}>
+          Coach: {coach.name}
+        </Typography>
+        <Typography variant="body1">Phone:</Typography>{" "}
+        <Typography variant="body2" sx={{ marginBottom: 2 }}>
+          {coach.phone}
+        </Typography>
+        {coach.website && (
+          <>
+            <Typography variant="body1">Website:</Typography>
+            <Typography variant="body2" sx={{ marginBottom: 2 }}>
+              {coach.website}
+            </Typography>{" "}
+          </>
+        )}
+        <Typography variant="body1">Email:</Typography>
+        <Typography variant="body2" sx={{ marginBottom: 2 }}>
+          {coach.email}
+        </Typography>{" "}
+        <Typography variant="body1" sx={{ marginBottom: 1 }}>
+          Specialties:
+        </Typography>
+        <Box>
+          {coach.specialties?.map((specialty) => {
+            return (
+              <Chip
+                icon={<SportsFootballIcon />}
+                key={specialty.id}
+                label={specialty.name}
+                component="a"
+                variant="outlined"
+                sx={{ height: 32 }}
+                onDelete={(e) => handleRemove(specialty.id)}
+              />
+            );
+          })}
+        </Box>
+        <Button
+          variant="contained"
+          size="large"
+          sx={{
+            maxWidth: 300,
+            marginTop: 4,
+            marginBottom: 4,
+          }}
+        >
+          <UpdateIcon sx={{ marginRight: 1 }} />
+          Update Coach Profile
+        </Button>
+      </Container>
     </Container>
   );
 };
